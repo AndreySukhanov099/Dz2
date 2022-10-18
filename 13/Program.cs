47acc157b9346b3cc2a3b495d5a6b3f5ebfef5fc
And_Sukhanov <Andrey.sx@mail.ru>
2022-10-19 00:22:45 +0300

    Домашнее задание 2

---
+++ 13/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int GetThreedigit(int number)
+{
+    if (number < 1000)
+    {
+    int ThreeDigit = number % 10;
+    return ThreeDigit;
+    }
+    else 
+    {
+        while(number > 999)
+            number = number/10;
+        int ThreeDigit = number % 10;
+        return ThreeDigit;
+    }
+}
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+if (number < 100)
+Console.WriteLine("третьей цифры нет");
+else
+{
+int ThreeDigit = GetThreedigit(number);
+Console.WriteLine(ThreeDigit);
+}
---
+++ 15/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер день недели");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if(num1 < 8)
+{
+    if(num1 < 6)
+    {
+    System.Console.WriteLine("нет");
+    }
+    else
+    {
+    System.Console.WriteLine("Да");
+    }
+}
+else
+{
+System.Console.WriteLine("Введенного дня недели не существует");
+}
